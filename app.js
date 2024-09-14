2525eb8b02e5ea2a992e44fe9beef3b0bf3a3de4
Nkambenhle Zungu <129110008+nkambenhle@users.noreply.github.com>
2024-09-14 12:43:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,29 @@
+ // app.js
 const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const setupSwagger = require('./swagger');
 
-// GET all users
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/users', require('./routes/userRoutes'));
+app.use('/assignments', require('./routes/assignmentRoutes'));
+app.use('/submissions', require('./routes/submissionRoutes'));
+app.use('/feedbacks', require('./routes/feedbackRoutes'));
+app.use('/modules', require('./routes/moduleRoutes'));
+
+// Setup Swagger
+setupSwagger(app);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-// GET user by ID
-router.get('/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (user) res.json(user);
-        else res.status(404).json({ message: 'User not found' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-// POST create a new user
-router.post('/', async (req, res) => {
-    const user = new User(req.body);
-    try {
-        const newUser = await user.save();
-        res.status(201).json(newUser);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-// PATCH update a user
-router.patch('/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (user) res.json(user);
-        else res.status(404).json({ message: 'User not found' });
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-// DELETE a user
-router.delete('/:id', async (req, res) => {
-    try {
-        const result = await User.findByIdAndDelete(req.params.id);
-        if (result) res.json({ message: 'User deleted' });
-        else res.status(404).json({ message: 'User not found' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-module.exports = router;
-
-
-
