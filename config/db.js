a5f42b6084ded4ab85684b293029600ae21afc2b
Nkambenhle Zungu <129110008+nkambenhle@users.noreply.github.com>
2024-09-14 12:48:58 +0200

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,22 @@
+ 
+// config/db.js
+const mongoose = require('mongoose');
+
+mongoose.set('debug', true);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://nkambenhle17zungu:AQnjaI8M0kGZzVyq@hms.ohajons.mongodb.net/?retryWrites=true&w=majority&appName=HMS', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+
+module.exports = connectDB;
+
