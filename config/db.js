3b2bb4707da35ac754ea2915bd524bc97aad030f
tomstyll <144017585+tomstyll@users.noreply.github.com>
2024-09-18 13:50:36 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,21 +2,18 @@
 // config/db.js
 const mongoose = require('mongoose');
 
-mongoose.set('debug', true);
-
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://nkambenhle17zungu:AQnjaI8M0kGZzVyq@hms.ohajons.mongodb.net/?retryWrites=true&w=majority&appName=HMS', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        console.log('MongoDB connected');
+        console.log('MongoDB connected'); // Log success message
     } catch (err) {
-        console.error(err.message);
-        process.exit(1);
+        console.error(err.message); // Log error message
+        process.exit(1); // Exit the process with failure
     }
 };
 
-
-module.exports = connectDB;
+module.exports = connectDB; // Export the function
 
