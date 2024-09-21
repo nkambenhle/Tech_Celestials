0aeb98d11fe3b6bbf39cc0608730f13736adb6cf
tomstyll <144017585+tomstyll@users.noreply.github.com>
2024-09-21 10:26:26 +0200

    Update assignment.js

--- models/assignment.js
+++ models/assignment.js
@@ -13,3 +13,20 @@ const AssignmentSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('Assignment', AssignmentSchema);
 
+
+//This is another idea of doing it
+const mongoose = require('mongoose');
+
+const AssignmentSchema = new mongoose.Schema({
+    
+    Title: String,
+    Description: String,
+    Marks: Number,
+    Open_Date: Date,
+    Due_Date: Date,
+    Module_ID: { type: mongoose.Schema.Types.ObjectId, ref: 'Module' } // References Module collection in mongodb
+    
+});
+
+module.exports = mongoose.model('Assignment', AssignmentSchema);
+
