0b68d66e2bed011e26b6ab97d139ef02a7209ea0
tomstyll <144017585+tomstyll@users.noreply.github.com>
2024-09-26 01:33:48 +0200

    Update feedback.js

--- models/feedback.js
+++ models/feedback.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const FeedbackSchema = new mongoose.Schema({
-    Feedback_ID: Number,
-    Submission_ID: Number,
-    Feedback_Text: String,
-    Marks: Number,
-    User_ID: Number
+    Submission_ID: { type: mongoose.Schema.Types.ObjectId, ref: 'Submission', required: true },
+    Feedback_Text: { type: String, required: true },
+    Marks: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Feedback', FeedbackSchema);
