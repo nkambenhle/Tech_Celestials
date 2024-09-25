905a02ff481a26a0507cac2bac77e63fee2edda2
tomstyll <144017585+tomstyll@users.noreply.github.com>
2024-09-25 17:10:58 +0200

    Update submission.js

--- models/submission.js
+++ models/submission.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
+// Submission Schema with Assignment and User references
 const SubmissionSchema = new mongoose.Schema({
-    Submission_ID: Number,
-    Assignment_ID: Number,
-    User_ID: Number,
-    Video: String,
-    Date: Date
+    Assignment_ID: { type: mongoose.Schema.Types.ObjectId, ref: 'Assignment', required: true },
+    User_ID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    Video: { type: String, required: true }, // Store the filename in GridFS
+    Date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Submission', SubmissionSchema);
